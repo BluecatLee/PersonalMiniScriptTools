f23be4cd420a29e9a0714296e8bf026be7a459d2
BluecatLee <jiaobujun@163.com>
2019-07-26 13:44:20 +0800

    抽取方法 添加发布重启所有服务的功能

--- demo/publish.sh
+++ demo/publish.sh
@@ -1,40 +1,69 @@
 #!/bin/bash
 
-
-PARAM=$1
-
-if [[ "$PARAM" == "site" ]] || [[ "$PARAM" == "third" ]] || [[ "$PARAM" == "boss" ]]; then
-	PROJECT_NAME=yxcg-$PARAM
-	DEST_PATH=$PARAM
-elif [ "$PARAM" == "pay" ]; then
-	PROJECT_NAME=yegoo-pay-new
-	DEST_PATH=service
-else
-	PROJECT_NAME=yxcg-$PARAM-service
-	DEST_PATH=service
-fi
-
-#进入代码目录 获取最新代码
-PROJECT_PATH=/home/code/yxcg/$PROJECT_NAME
-cd $PROJECT_PATH && git pull
-
-#编译打包
-mvn clean install -DskipTests -f /home/code/yxcg/$PROJECT_NAME/pom.xml
-
-if [ $? -ne 0 ];
-then
-echo "error ,please reset"
-exit 0
-fi
-
-#除了common search模块 其余都是maven聚合工程 jar包在app模块下
-if [[ "$PARAM" == "common" ]] || [[ "$PARAM" == "search" ]]; then
-	SRC=$PROJECT_PATH/target/*.jar
-else 
-	SRC=$PROJECT_PATH/app/target/*.jar
-fi
-DES=/data/yxcg/$DEST_PATH
 HOST=medical-dev
 
-#传到远程服务器
-ssh root@$HOST "rm -rf $DES/$PROJECT_NAME'.jar'" && scp $SRC root@$HOST:$DES/$PROJECT_NAME".jar"
+function publish() {
+	PARAM=$1
+
+	echo "==== Starting publish $PARAM ===="
+	
+	if [[ "$PARAM" == "site" ]] || [[ "$PARAM" == "third" ]] || [[ "$PARAM" == "boss" ]]; then
+       		PROJECT_NAME=yxcg-$PARAM
+        	DEST_PATH=$PARAM
+	elif [ "$PARAM" == "pay" ]; then
+        	PROJECT_NAME=yegoo-pay-new
+        	DEST_PATH=service
+	else
+        	PROJECT_NAME=yxcg-$PARAM-service
+        	DEST_PATH=service
+	fi
+
+	#进入代码目录 获取最新代码
+	PROJECT_PATH=/home/code/yxcg/$PROJECT_NAME
+	cd $PROJECT_PATH && git pull
+
+	#编译打包
+	mvn clean install -DskipTests -f /home/code/yxcg/$PROJECT_NAME/pom.xml
+
+	if [ $? -ne 0 ];
+	then
+	echo "error occured when publish $PARAM ,please reset"
+	exit 0
+	fi
+
+	#除了common search模块 其余都是maven聚合工程 jar包在app模块下
+	if [[ "$PARAM" == "common" ]] || [[ "$PARAM" == "search" ]]; then
+        	SRC=$PROJECT_PATH/target/*.jar
+	else
+        	SRC=$PROJECT_PATH/app/target/*.jar
+	fi
+	DES=/data/yxcg/$DEST_PATH
+
+	#传到远程服务器
+	ssh root@$HOST "rm -rf $DES/$PROJECT_NAME'.jar'" && scp $SRC root@$HOST:$DES/$PROJECT_NAME".jar"
+
+	echo "==== Success publish $PARAM ===="
+}
+
+#all
+if [ ! -n "$1" ]; then
+	echo "publish error: params cannot be empty!"
+elif [ "$1" == "all" ]; then
+
+	publish 'common'	
+	publish 'goods'
+	publish 'order'
+	publish 'supplier'
+	publish 'purchaser'
+	publish 'info'
+	publish 'pay'
+	publish 'search'
+	publish 'platform'
+
+	publish 'site'
+	publish 'third'
+	publish 'boss'
+
+else 
+	publish $1
+fi
--- demo/restart.sh
+++ demo/restart.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 
-PARAM=$1
+function restart() {
+	PARAM=$1
 
-if [[ "$PARAM" == "site" ]] || [[ "$PARAM" == "third" ]] || [[ "$PARAM" == "boss" ]]; then
-	RELEASE=/data/yxcg/$PARAM/yxcg-$PARAM'.jar'
-elif [ "$PARAM" == "pay" ]; then
-	RELEASE=/data/yxcg/service/yegoo-pay-new.jar
-else
-	RELEASE=/data/yxcg/service/yxcg-$PARAM-service.jar
-fi
+	if [[ "$PARAM" == "site" ]] || [[ "$PARAM" == "third" ]] || [[ "$PARAM" == "boss" ]]; then
+        	RELEASE=/data/yxcg/$PARAM/yxcg-$PARAM'.jar'
+	elif [ "$PARAM" == "pay" ]; then
+        	RELEASE=/data/yxcg/service/yegoo-pay-new.jar
+	else
+        	RELEASE=/data/yxcg/service/yxcg-$PARAM-service.jar
+	fi
 
-if [ "$PARAM" == "search" ]; then
-	PID=`ps axu | grep $PARAM'-service' | grep java | awk '{print $2}'`
+	if [ "$PARAM" == "search" ]; then
+        	PID=`ps axu | grep $PARAM'-service' | grep java | awk '{print $2}'`
+	else 
+        	PID=`ps axu | grep $1 | grep java | awk '{print $2}'`
+	fi
+	echo $PID
+	kill -9 $PID
+
+	sleep 5
+
+	nohup java -jar ${RELEASE} >> /data/yxcg/log/$PARAM'.log' 2>&1 &
+
+}
+
+if [ ! -n "$1" ]; then 
+	echo "restart error: params cannot be empty!"
+elif [ "$1" == "all" ]; then
+	restart 'common'
+	restart 'goods'
+	restart 'order'
+	restart 'supplier'
+	restart 'purchaser'
+	restart 'pay'
+	restart 'info'
+	restart 'pay'
+	restart 'search'
+	restart 'platform'
+
+	restart 'site'
+	restart 'third'
+	restart 'boss'
 else 
-	PID=`ps axu | grep $1 | grep java | awk '{print $2}'`
+	restart $1
 fi
-echo $PID
-kill -9 $PID
 
-sleep 5
 
-#RELEASE=`find  /data/marking -type f | egrep "app.*jar"`
-
-nohup java -jar ${RELEASE} >> /data/yxcg/log/$PARAM'.log' 2>&1 &
