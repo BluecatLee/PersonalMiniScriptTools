ad79b2b8fd5f2c45d9dde0864c1a1a9266f6f5bf
BluecatLee <jiaobujun@163.com>
2019-08-02 16:11:20 +0800

    添加前端vue项目发布脚本

---
+++ demo/publish-web.sh
@@ -0,0 +1,37 @@
+﻿#!/bin/bash
+
+PARAM=$1
+
+HOST=medical-dev
+
+function publishVueProject() {
+
+	PROJECT_NAME=$1
+
+	REMOTE_DIR=$2
+
+	cd /home/code/yxcg/$PROJECT_NAME && git pull
+
+	sleep 2
+
+	npm install 
+
+	node ./build/build.js 
+
+	scp -r ./dist/* root@$HOST:$REMOTE_DIR
+
+}
+
+
+if [ ! -n "$1" ]; then
+	echo "publish error: params cannot be empty!"
+elif [ "$PARAM" == "third" ]; then
+	echo "not supported now!"
+elif [ "$PARAM" == "boss" ]; then 
+	PROJECT_NAME=yegoo-consumables-boss-web
+	REMOTE_DIR=/data/yxcg/boss/static/
+	publishVueProject $PROJECT_NAME $REMOTE_DIR
+else
+	echo "error params"
+fi
+
