35ec9eb88b5c49d811d1f6ffe4856d3dc015c456
BluecatLee <jiaobujun@163.com>
2019-08-06 18:35:43 +0800

    add ftp

---
+++ ftp/fetch-file.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+
+HOST=192.168.10.131
+USER=root
+PASS=pass
+REMOTE_DIR=/home
+LOCAL_DIR=/tmp
+FILE=testfile
+
+ftp -n <<! 
+open $HOST
+user $USER $PASS
+passive
+binary
+cd $REMOTE_DIR
+lcd $LOCAL_DIR
+prompt
+get $FILE
+close
+bye
+!
