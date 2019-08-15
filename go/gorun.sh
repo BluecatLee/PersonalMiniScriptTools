f9d5a94067287b697b47d5df61290f2e5d9b3546
BluecatLee <jiaobujun@163.com>
2019-08-15 18:27:50 +0800

    add go

---
+++ go/gorun.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PARAM=$1
+
+CURRENT_DIR=`pwd`
+OLD_GO_PATH="$GOPATH"  #例如: /usr/local/go
+OLD_GO_BIN="$GOBIN"    #例如: /usr/local/go/bin
+
+export GOPATH="$CURRENT_DIR" 
+export GOBIN="$CURRENT_DIR/bin"
+
+#指定并整理当前的源码路径
+gofmt -w src
+
+go install $PARAM
+
+#echo "测试"
+
+export GOPATH="$OLD_GO_PATH"
+export GOBIN="$OLD_GO_BIN"
